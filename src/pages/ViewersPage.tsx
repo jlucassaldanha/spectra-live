1fcf15b6caff0bf01add80a2e02074e1c3e872d1
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-08-14 13:10:35 -0300

    Update ViewersPage.tsx

--- src/pages/ViewersPage.tsx
+++ src/pages/ViewersPage.tsx
@@ -20,26 +20,6 @@ function ViewersPage(){
 	const moderator_id: string = ""
 	const broadcaster_id: string = ""
 
-	{/*const users = [
-		{
-			name: "teste1",
-			img: "https://yt3.googleusercontent.com/ytc/AIdro_l-U0s4DusUoDq-0X9v_yvheRNXoy1jZfK7CogXkJcpz7Q=s120-c-k-c0x00ffffff-no-rj"
-		},
-		{
-			name: "teste2",
-			img: "https://yt3.googleusercontent.com/bnj1afxDUwz-_GbNZQa_9M45U55BuPiXczws1qmYWcy5uQCoppf7k_Bacq3AUP2sHGiMkNxhkw=s120-c-k-c0x00ffffff-no-rj"
-		},
-		{
-			name: "3teste",
-			img: "https://yt3.googleusercontent.com/trTOeasBYL_IxmwM8GpAT4_fXl1XZ9tUvxlUplzSFgBa_n3qcgcYOQ8oSh80rPdYZODCXD-Wag=s120-c-k-c0x00ffffff-no-rj"
-		},
-		{
-			name: "4teste",
-			img: "https://yt3.googleusercontent.com/ytc/AIdro_mvK5CpITy5tDYSu3iH3dgUu1iMu47xCdTVby0NLU3rsg=s120-c-k-c0x00ffffff-no-rj"
-		},
-		
-	]*/}
-
 	type ChatterType = {
 		user_id: string
 		user_login: string
