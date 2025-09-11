7ac1e9e5e97a729c0b533484fffbffdd37554408
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-09-11 12:56:32 -0300

    Update DashboardPage.tsx
    Obtendo informações do usuário

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,11 +1,22 @@
+import { useEffect, useRef, useState } from "react"
 import Button from "../components/Button/Button"
 import ServerApi  from "../utils/ServerApi"
 
+type UserDataType = {
+    display_name: string,
+    profile_image_url: string
+}
+
 function DashboardPage(){
+   const [userData, setUserData] = useState<UserDataType>()
+
+    const calledRef = useRef(false)
+
     const handleClick = () => {
         ServerApi.post("/auth/logout")
             .then((response) => {
                console.log(response)
+               setUserData(response.data)
             })
             .catch((error) => {
                 console.log(error)
@@ -14,9 +25,28 @@ function DashboardPage(){
         window.location.href = "http://localhost:5173/home"
     }
 
+    useEffect(() => {
+        if (calledRef.current) return
+        calledRef.current = true
+
+        ServerApi.get("/auth/me", {withCredentials: true})
+            .then((response) => {
+               console.log(response.data)
+               setUserData(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
+
     return (
         <div>
-            aaaaa
+            {userData != undefined && (
+                <div>
+                    {userData.display_name} <br />
+                    {/*<img src={userData.profile_image_url} alt="Foto de perfil" />*/}
+                </div>
+            )}
             <Button onClick={handleClick}>
                 Sair
             </Button>
