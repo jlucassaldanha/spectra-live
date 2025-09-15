a1c33da1f53725209ccdbdc4771003ba52d2e92c
João Saldanha <jlucassaldanha@gmail.com>
2025-09-15 20:21:15 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const ROOT_URL = "https://spectralive.vercel.app"
-export const API_URL = "https://spectra-api.onrender.com"
+export const API_URL = "https://spectra-server-production.up.railway.app"
